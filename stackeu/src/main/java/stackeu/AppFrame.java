3d6648cad82562e376b5b5c6c246d2e1bec3f926
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-03-31 15:25:07 +0800

    integrated new files with the exisiting ones

--- stackeu/src/main/java/stackeu/AppFrame.java
+++ stackeu/src/main/java/stackeu/AppFrame.java
@@ -1,57 +1,25 @@
 package stackeu;
 
-import javafx.concurrent.Worker;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import netscape.javascript.JSObject;
 
 public class AppFrame extends StackPane {
-    private AppBridge bridge;
-    private WebView homeView; 
-
-    public AppFrame(Stage stage){
-        bridge = new AppBridge(this, stage);
-        
-        // Only load the index.html for now
-        homeView = createWebView("/index.html");
-        this.getChildren().add(homeView);
-    }
-
-    private WebView createWebView(String path) {
+    
+    public AppFrame(Stage stage) {
         WebView webView = new WebView();
         
-        webView.getEngine().getLoadWorker().stateProperty().addListener((obs, oldState, newState) -> {
-            if (newState == Worker.State.SUCCEEDED) {
-                JSObject window = (JSObject) webView.getEngine().executeScript("window");
-                // Injects the AppBridge object into JS as 'javaApp'
-                window.setMember("javaApp", bridge); 
-            }
-        });
+        // Enable JavaScript and LocalStorage
+        webView.getEngine().setJavaScriptEnabled(true);
         
-        java.net.URL resource = getClass().getResource(path);
+        // Load the initial index.html
+        java.net.URL resource = getClass().getResource("/index.html");
         if (resource != null) {
             webView.getEngine().load(resource.toExternalForm());
         } else {
-            System.err.println("CRITICAL ERROR: Could not find " + path);
+            System.err.println("CRITICAL ERROR: Could not find /index.html");
         }
         
-        return webView;
-    }
-
-    public void switchView(String viewName) {
-        if (homeView == null) return;
-
-        // Hide all views first
-        homeView.setVisible(false);
-
-        // Show the requested view
-        switch (viewName) {
-            case "home":  
-                homeView.setVisible(true);  
-                homeView.toFront();  
-                break;
-            // You can add "hiw" and "start" back here later
-        }
+        this.getChildren().add(webView);
     }
 }
