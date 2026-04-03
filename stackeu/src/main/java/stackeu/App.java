82839a49719cd6327cd9bd2bcd37722b1685dbeb
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-04-03 12:03:10 +0800

    added splashscreen

--- stackeu/src/main/java/stackeu/App.java
+++ stackeu/src/main/java/stackeu/App.java
@@ -3,8 +3,9 @@ package stackeu;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
-import javafx.stage.Stage;
+import javafx.stage.Stage; 
 
 public class App extends Application {
     
@@ -17,12 +18,17 @@ public class App extends Application {
         Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight()); 
         
         primaryStage.setTitle("Stackeu");
+        try {
+            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/logo.png")));
+        } catch (Exception e) {
+            System.err.println("Could not load app icon. Check path: /assets/logo.png");
+        }
         primaryStage.setScene(scene);
     
         primaryStage.setMaximized(true); 
         
         // setting up full screen
-        primaryStage.setFullScreen(true);
+        // primaryStage.setFullScreen(true);
         
         primaryStage.show();
     }
